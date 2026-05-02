4a5fc77e5d2ecadca59a202378669cd87314cfa7
mickegng <micke6649@gmail.com>
2026-05-02 20:59:32 +0200

    aktualizacja ogolnie, emailjs, seo, burger menu. Pozdrawiam.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -12,6 +11,4 @@ def kontakt():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-    
+    app.run(debug=True)
